0cc156c0572992e1d9bac9cd4aec5e62050f90ea
Sergio Sánchez <sss4esob@gmail.com>
2017-08-15 15:03:56 +0200

    feat(common): Add basic utilities.

---
+++ src/main/java/org/sanchez/sergio/fda/ingestion/platform/FlinkFDADocumentsStreamProcessor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sanchez.sergio.fda.ingestion.platform;
+
+/**
+ *
+ * @author sergio
+ */
+public class FlinkFDADocumentsStreamProcessor {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/org/sanchez/sergio/fda/ingestion/platform/config/DiPConfiguration.java
@@ -0,0 +1,19 @@
+package org.sanchez.sergio.fda.ingestion.platform.config;
+
+/**
+ *
+ * @author sergio
+ */
+public class DiPConfiguration {
+
+    public static final String CONFIG_FILE = "config";
+    public static final String DEFAULT_CONFIG_FILE = "/config.properties";
+
+    public static final String REWIND = "rewind";
+    public static final String KAFKA_TOPIC = "kafka.topic";
+    public static final String KAFKA_GROUP_ID = "group.id";
+    public static final String KAFKA_BOOTSTRAP_SERVERS = "bootstrap.servers";
+    public static final String ZK_HOST = "zookeeper.host";
+    public static final String ZK_PORT = "zookeeper.port";
+
+}
