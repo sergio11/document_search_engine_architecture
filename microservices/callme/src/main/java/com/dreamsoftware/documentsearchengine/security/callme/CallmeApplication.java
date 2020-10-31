96a4ab23e15cbf6729157c4f65708b6c78e32d79
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2020-10-31 15:35:10 +0100

    Add microservice API Gateway basend on Spring Cloud Gateway

---
+++ microservices/callme/src/main/java/com/dreamsoftware/documentsearchengine/security/callme/CallmeApplication.java
@@ -0,0 +1,13 @@
+package com.dreamsoftware.documentsearchengine.security.callme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallmeApplication.class, args);
+    }
+
+}
