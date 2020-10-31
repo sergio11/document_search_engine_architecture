feb97c8e4e88994b68a6ffdbeaca963352d4b18c
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2020-10-31 19:52:36 +0100

    Create microservices for files management, files metadata and files search

---
+++ microservices/files_management/src/main/java/com/dreamsoftware/documentsearchengine/security/callme/FilesManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dreamsoftware.documentsearchengine.security.callme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesManagementServiceApplication.class, args);
+    }
+
+}
--- microservices/files_metadata/src/main/java/com/dreamsoftware/documentsearchengine/security/callme/FilesMetadataServiceApplication.java
+++ microservices/files_metadata/src/main/java/com/dreamsoftware/documentsearchengine/security/callme/FilesMetadataServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CallmeApplication {
+public class FilesMetadataServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CallmeApplication.class, args);
+        SpringApplication.run(FilesMetadataServiceApplication.class, args);
     }
 
 }
