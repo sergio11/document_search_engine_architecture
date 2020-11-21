8d9109660daea2be2069d4b7ec986b33d813ab6b
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2020-11-21 20:52:56 +0100

    Add Files API Gateway

--- microservices/files_api_gateway/src/main/java/com/dreamsoftware/documentsearchengine/FilesApiGatewayApplication.java
+++ microservices/files_api_gateway/src/main/java/com/dreamsoftware/documentsearchengine/FilesApiGatewayApplication.java
@@ -1,28 +1,22 @@
-package com.dreamsoftware.documentsearchengine.security.gateway;
+package com.dreamsoftware.documentsearchengine;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.WebSession;
 
 @SpringBootApplication
 @RestController
-public class FilesGatewayApplication {
+public class FilesApiGatewayApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FilesGatewayApplication.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(FilesApiGatewayApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(FilesGatewayApplication.class, args);
+        SpringApplication.run(FilesApiGatewayApplication.class, args);
     }
 
-    @GetMapping(value = "/token")
+    /*@GetMapping(value = "/token")
     public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
         return Mono.just(authorizedClient.getAccessToken().getTokenValue());
     }
@@ -30,6 +24,5 @@ public class FilesGatewayApplication {
     @GetMapping("/")
     public Mono<String> index(WebSession session) {
         return Mono.just(session.getId());
-    }
-
+    }*/
 }
