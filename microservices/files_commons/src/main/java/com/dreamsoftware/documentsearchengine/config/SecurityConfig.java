627e3e92294b66e85547d4b7151158cf3a6a6e6a
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2020-12-19 23:02:23 +0100

    Solve issues

--- microservices/files_commons/src/main/java/com/dreamsoftware/documentsearchengine/config/SecurityConfig.java
+++ microservices/files_commons/src/main/java/com/dreamsoftware/documentsearchengine/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.dreamsoftware.documentsearchengine.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -17,24 +16,36 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST_SWAGGER_V2 = {
+        // -- swagger ui
+        "/v2/api-docs",
+        "/swagger-resources/**",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui.html",
+        "/webjars/**"
+    };
+
+    private static final String[] AUTH_WHITELIST_SWAGGER_V3 = {
+        // -- swagger ui
+        "/v2/api-docs",
+        "/v2/api-docs/**",
+        "/v3/api-docs",
+        "/v3/api-docs/**",
+        "/swagger-resources/**",
+        "/swagger-ui/**",};
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize
                 -> authorize.antMatchers("/actuator/**")
                         .permitAll()
+                        .antMatchers(AUTH_WHITELIST_SWAGGER_V2).permitAll()
+                        .antMatchers(AUTH_WHITELIST_SWAGGER_V3).permitAll()
                         .anyRequest()
                         .authenticated()
         )
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v3/api-docs",
-                "/api-docs/**",
-                "/api-docs/swagger-config",
-                "/swagger-ui.html",
-                "/swagger-ui/**");
     }
 
 }
