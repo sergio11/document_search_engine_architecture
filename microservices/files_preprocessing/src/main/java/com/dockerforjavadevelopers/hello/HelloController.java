66caf62b4bf9670bcc7ba5b78d711721b6c580b5
nmassaria <nmassaria@protonmail.com>
2021-11-23 10:05:46 -0500

    trying to fix file uploads then connect to nifi

--- microservices/files_preprocessing/src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ microservices/files_preprocessing/src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -3,13 +3,27 @@ package com.dockerforjavadevelopers.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
+
+
+
+
+// TEST CLASS , NOT ACTUAL PREPROCESSING CLASS (which is in another project for now)
 @RestController
 public class HelloController {
-    
+
+
+
     @RequestMapping("/")
     public String index() {
         return "Hello World\n";
+    }
+
+    @PutMapping("/")
+    public String index(){
+        //System.out.println("!!!!!!!!!!!!!!!!!WORKED!!!!!!!!!!!");
+        return "worked...\n";
     }
     
 }
